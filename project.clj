d9f21f7f60605ad14bd4fb2a64e3c3c068a32983
Daniel Compton <desk@danielcompton.net>
2016-05-24 21:14:38 +1200

    Add clojars deploy repositories

--- project.clj
+++ project.clj
@@ -5,5 +5,7 @@
             :url "https://opensource.org/licenses/MIT"}
   :global-vars {*warn-on-reflection* true}
   :dependencies [[org.clojure/clojure "1.8.0"]]
+  :deploy-repositories [["releases" :clojars]
+                        ["snapshots" :clojars]]
   :profiles {:dev {:dependencies [[criterium "0.4.4"]
                                   [ring/ring-mock "0.3.0"]]}})
