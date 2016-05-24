957cdbcfb9a638d9b8fdd90f6ae47d663f768776
Daniel Compton <desk@danielcompton.net>
2016-05-24 21:13:07 +1200

    Fix license in project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject net.danielcompton/ring-ip-whitelisting "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
   :global-vars {*warn-on-reflection* true}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :profiles {:dev {:dependencies [[criterium "0.4.4"]
