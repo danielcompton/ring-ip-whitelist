492dfb5eba472b7175010760796b356f9ad9bcee
Daniel Compton <desk@danielcompton.net>
2016-06-10 14:36:47 +1200

    Version 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.danielcompton/ring-ip-whitelist "0.2.0-SNAPSHOT"
+(defproject net.danielcompton/ring-ip-whitelist "0.2.0"
   :description "Ring IP whitelisting middleware"
   :url "http://github.com/danielcompton/ring-ip-whitelist"
   :license {:name "MIT License"
